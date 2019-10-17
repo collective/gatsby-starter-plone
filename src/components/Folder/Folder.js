e816360ca70f4428c7ae3b98c7f4322fd4b6b00b
Asko Soukka <asko.soukka@iki.fi>
2019-10-17 21:34:45 +0300

    Add support for displaying images on the front page

--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -29,7 +29,9 @@ const Folder = ({ data, title, images = [], files = [] }) => {
       <ul className="list-group">
         {data.items
           .filter(
-            item => listedTypes.has(item._type) && item._path !== '/docs/index/'
+            item =>
+              listedTypes.has(item._type) &&
+              !['/frontpage/', '/front-page/'].includes(item._path)
           )
           .map(item => (
             <li key={item._path} className="list-group-item">
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,15 @@ import Layout from '../components/Layout';
 import Document from '../components/Document';
 import Folder from '../components/Folder';
 
+const nodes = query => (query ? query['edges'] : []).map(edge => edge.node);
+
 const IndexPage = ({ data }) => (
   <Layout>
-    <Document data={data.ploneDocument} />
+    <Document
+      data={data.ploneDocument}
+      images={nodes(data['allPloneImage'])}
+      files={nodes(data['allPloneFile'])}
+    />
     <hr style={{ background: '#e8eef2' }} />
     <Folder data={data.ploneSite} title="Contents" />
   </Layout>
@@ -17,11 +23,25 @@ export default IndexPage;
 
 export const query = graphql`
   query IndexPageQuery {
-    ploneDocument(_path: { eq: "/frontpage/" }) {
+    ploneDocument(_path: { in: ["/frontpage/", "/front-page/"] }) {
       ...Document
     }
     ploneSite(_path: { eq: "/" }) {
       ...Site
+    }
+    allPloneFile(filter: { _backlinks: { eq: "/frontpage/" } }) {
+      edges {
+        node {
+          ...File
+        }
+      }
+    }
+    allPloneImage(filter: { _backlinks: { eq: "/frontpage/" } }) {
+      edges {
+        node {
+          ...Image
+        }
+      }
     }
   }
 `;
