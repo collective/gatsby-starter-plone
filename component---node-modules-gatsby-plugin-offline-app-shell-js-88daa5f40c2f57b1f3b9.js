9a8b976f2abfb8c72994cc33e6a66a7d58e7a620
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-21 13:31:24 +0000

    Deploy collective/gatsby-starter-plone to github.com/collective/gatsby-starter-plone.git:gh-pages

---
+++ component---node-modules-gatsby-plugin-offline-app-shell-js-88daa5f40c2f57b1f3b9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{221:function(e,t,n){"use strict";var u=n(18);t.__esModule=!0,t.default=void 0;var o=u(n(60)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-88daa5f40c2f57b1f3b9.js.map
---
+++ component---src-pages-404-js-5eeb704f0aebe897eafb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),l=n("Zttt");e.default=function(){return u.a.createElement(l.a,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5eeb704f0aebe897eafb.js.map
